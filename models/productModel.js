cb2723ec413c53ec1dc11c58b30972167690c88a
Joy Agbo <joyagbo49@gmail.com>
2023-10-14 00:18:27 +0100

    added product model

---
+++ models/productModel.js
@@ -0,0 +1,39 @@
+const mongoose = require("mongoose");
+
+const productSchema = new mongoose.Schema({
+    recyclableMatrial:{
+        type: String,
+        required: true
+    },
+    quantity: {
+        type: String,
+        required: true
+    },
+    volume: {
+        type: String,
+        required: true
+    },
+    price: {
+        type: String,
+        required: false
+    },
+    location: {
+        type: String,
+        required: true
+    },
+    
+    pictureUrl: {
+        type: String,
+        required: true
+    },
+    createdBy: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "user",
+        required: true,
+    },
+
+});
+
+const Product = mongoose.model("project", productSchema);
+
+module.exports = { Product };
