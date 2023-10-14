8eb6eedfb5b9b671245baa6c358edadc9bd4100f
Joy Agbo <joyagbo49@gmail.com>
2023-10-14 11:18:49 +0100

    formatted file

--- app.js
+++ app.js
@@ -1,43 +1,45 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const cors = require("cors");
 const mongoose = require("mongoose");
 require("dotenv").config();
-const router = require("./routes/userRoute");
-const cookieParser = require('cookie-parser');
-//const projectRoutes = require("./routes/projectRoutes");
+const userRouter = require("./routes/userRoute");
+const cookieParser = require("cookie-parser");
+const productRoute = require("./routes/productRoute");
 
 const app = express();
+app.use(cors());
 
 //middleware
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.json({ extended: false }));
 app.use(cookieParser());
 
-
 //   Api Health Checker
 app.get("/api/healthchecker", (req, res) => {
-    res.status(200).json({
-        status: "success",
-        message: "Welcome to Green Reward",
-    });
+  res.status(200).json({
+    status: "success",
+    message: "Welcome to Green Reward",
+  });
 });
 
 //userRoute router
-app.use("/users", router);
-// app.use("/projects", projectRoutes);
+app.use("/users", userRouter);
 
+//productRoute
+app.use("/products", productRoute);
 
 //Database connection
 const dbURI = process.env.MONGO_URI;
-mongoose.connect(dbURI, {
+mongoose
+  .connect(dbURI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-})
-    .then(() => console.log("database connected"))
-    .catch((err) => console.log(err));
-
+  })
+  .then(() => console.log("database connected"))
+  .catch((err) => console.log(err));
 
 const port = process.env.PORT || 8080;
 app.listen(port, () => {
-    console.log(`server running on port ${port}`);
+  console.log(`server running on port ${port}`);
 });
