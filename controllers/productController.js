4aedb02aa9e3d4d380ec247fef42e1ab04c3c566
Joy Agbo <joyagbo49@gmail.com>
2023-10-23 20:51:46 +0100

    added edit logic

--- controllers/productController.js
+++ controllers/productController.js
@@ -50,28 +50,38 @@ exports.createProduct = async (req, res) => {
   }
 };
 
-exports.editProduct = async (req, res) => {
-  if (!req.body) {
-    res.status(400).send({
-      message: "Content can not be empty!",
+module.exports.editProduct = async (req, res) => {
+  const updateData = req.body
+
+  try{
+    const productId = req.params.id
+    
+    if(!productId || !updateData){
+      return res.status(400).json({
+        status:"fail",
+        message:"Both productId and updateData are required."
+      })
+    }
+    const updatedProduct = await Product.findByIdAndUpdate(
+      productId, updateData,{new:true}
+    )
+    if(!updatedProduct){
+      return res.status(404).json({
+        Status:"fail",
+        message: "Product with not found."
+      })
+    }
+    res.status(200).json({
+      status:"success",
+      data: updatedProduct,
+    })
+  }catch(err){
+    console.log(err);
+    res.status(400).json({
+      status: "fail",
+      error: err.message,
     });
   }
-  Product.findByIdAndUpdate(req.params.id, { ...req.body }, (err, data) => {
-    if (err) {
-      if (err.kind === "not_found") {
-        res.status(404).send({
-          message: `Not found product with id ${req.params.id}.`,
-        });
-      } else {
-        res.status(500).send({
-          message: "Error editing with id " + req.params.id,
-        });
-      }
-    } else
-      res.status(200).send({
-        message: `product edited successfully.`,
-      });
-  });
 };
 
 exports.deleteProduct = async (req, res) => {
